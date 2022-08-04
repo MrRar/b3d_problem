f9881b8a98e7f372539796e00dc439a49dd710f9
Johannes Fritz <mr.rar1998@gmail.com>
2022-08-04 09:54:38 -0500

    First commit

---
+++ init.lua
@@ -0,0 +1,6 @@
+minetest.register_node("b3d_problem:boxes", {
+	mesh = "boxes.b3d",
+	description = "Boxes",
+	drawtype = "mesh",
+	tiles = {"default_lava.png", "default_cactus_top.png", "default_diamond_block.png"},
+})
